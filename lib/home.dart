888cb9a76fb2bb26524220ac5d1d38ba7d4cb70f
saliq03 <javidsaliq@gmail.com>
2024-07-25 21:54:22 +0530

    get api integrated

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+import 'package:rest_api_practice/jsonmodel.dart';
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -8,6 +11,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+ List<JsonModel> mydata=[];
+  FetchData()async{
+    String Url="https://jsonplaceholder.typicode.com/photos";
+    var response=await http.get(Uri.parse(Url));
+    var jsondata=jsonDecode(response.body.toString());
+    if(response.statusCode==200){
+      for(Map data in jsondata){
+        mydata.add(JsonModel.fromJson(data));
+      }
+      return mydata;
+    }
+    else{
+      return mydata;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
